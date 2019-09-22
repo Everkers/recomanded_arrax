2e2db787b21872862b2daa85419eecb7ba4119db
wael <waelkourich12@gmail.com>
2019-09-22 13:52:52 +0100

    fix issues

--- controllers/routes/uploadSong.js
+++ controllers/routes/uploadSong.js
@@ -4,21 +4,23 @@ const express =require('express'),
       isExist = require('../../utils/isExist') // check if song exists on the db 
 
 router.post('/' , (req , res)=>{ // handle post 
-    const {artist , name ,artwork}  = req.body;
-    if(!artist , !name , !artwork){ //check if there is no parameters
-        res.status(400).send({message:'the params is required please add them and try again'})
-    }
-    let findIt = null;
-    isExist(req.body).then(result=>{ //get the result from isexist function to check if the song exists or not
-        findIt = result
-    }).then(()=>{
-        if(findIt){ //checking exists value
-            res.status(400).send({message:'this song is already on our database'})
+         const {artist , name ,artwork}  = req.body.data;
+        if(!artist , !name , !artwork){ //check if there is no parameters
+            res.send({message:'the params is required please add them and try again'})
         }
-        else{ //The song is not exists yet so it should added to the database
-            res.send({message:'the song has been added to the database!'})
-            addSong(req.body)
-        }
-    })
+        let findIt = null;
+        isExist(req.body.data).then(result=>{ //get the result from isexist function to check if the song exists or not
+            findIt = result
+        }).then(()=>{
+            if(findIt){ //checking exists value
+                res.send({message:'this song is already on our database'})
+            }
+            else{ //The song is not exists yet so it should added to the database
+                res.send({message:'the song has been added to the database!'})
+                addSong(req.body.data)
+            }
+        })
+     
+
 });
 module.exports = router
